363082325653cd9628bcb1342b36134ce124f4cd
aTyro1 <2022amankumar@gmail.com>
2024-01-05 10:58:09 +0530

    commit from aman's laptop

---
+++ blogs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+def home(request):
+    home=loader.get_template('home.html')
+    return HttpResponse(home,render({}))
